bf132e0edc445527e256fe24abb49d4d39419fa7
rohbdn07 <rohbdn07@users.noreply.github.com>
2020-04-20 12:01:45 +0300

    script file added.

---
+++ script.js
@@ -0,0 +1,22 @@
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function () {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+      $("#back2Top").fadeIn();
+      $("#flex").fadeIn();
+    } else {
+      $("#back2Top").fadeOut();
+      $("#flex").fadeOut();
+    }
+  });
+  $(document).ready(function () {
+    $("#back2Top").click(function (event) {
+      event.preventDefault();
+      $("html, body").animate({
+          scrollTop: 0
+        },
+        "slow"
+      );
+      return false;
+    });
+});
